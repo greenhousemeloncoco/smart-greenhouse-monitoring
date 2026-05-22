36d591166960fd0830f3598d4b5a70160dadc504
hvarparfume-byte <hvarparfume@gmail.com>
2026-05-23 03:10:49 +0700

    fix railway mysql connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,24 +1,25 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
+
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-pool.getConnection()
-    .then(conn => {
-        console.log('✅ MySQL Connected');
-        conn.release();
-    })
-    .catch(err => {
+pool.getConnection((err, connection) => {
+    if (err) {
         console.error('❌ DB ERROR:', err);
-    });
+    } else {
+        console.log('✅ MySQL Connected');
+        connection.release();
+    }
+});
 
-module.exports = pool;
+module.exports = pool.promise();
